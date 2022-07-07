128fb0d19e62650f132972f9b60a1ed1206e6c22
godu <arthur.weber@viacesi.fr>
2022-07-07 11:51:37 +0200

    Modules

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
