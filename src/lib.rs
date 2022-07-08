225767f7c3e11ea78ed6c9b577218e84eaff8278
godu <arthur.weber@viacesi.fr>
2022-07-08 15:50:00 +0200

    Lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,33 @@ mod front_of_house {
     }
 }
 
+use crate::front_of_house::hosting::add_to_waitlist;
+pub use crate::front_of_house::hosting::add_to_waitlist as add_to_waitlist_2;
+
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
-
     front_of_house::hosting::add_to_waitlist();
+
+    add_to_waitlist();
+    add_to_waitlist_2();
 }
 
+#[allow(dead_code)]
 fn deliver_order() {}
 
 mod back_of_house {
+    #[allow(dead_code)]
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order();
     }
 
     fn cook_order() {}
+}
+
+use rand::Rng;
+
+pub fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("my secret number : {}", secret_number)
 }
